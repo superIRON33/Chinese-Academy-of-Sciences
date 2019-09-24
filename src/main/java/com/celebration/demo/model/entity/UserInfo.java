dcb5fe5510b38c111ae4470b746c4f4bb623b374
superIRON33 <1240490768@qq.com>
2019-09-24 21:02:47 +0800

    基本框架搭建

---
+++ src/main/java/com/celebration/demo/model/entity/UserInfo.java
@@ -0,0 +1,52 @@
+package com.celebration.demo.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user_info")
+public class UserInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "learn_time")
+    private Integer learnTime;
+
+    @Column(name = "institute")
+    private String institute;
+
+    @Column(name = "degree")
+    private String degree;
+
+    @Column(name = "workspace")
+    private String workspace;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "telephone")
+    private String telephone;
+
+    @Column(name = "email_add")
+    private String emailAdd;
+
+    @Column(name = "wechat_png")
+    private String wechatPNG;
+
+    @Column(name = "slogan")
+    private String slogan;
+
+
+}
---
+++ src/main/java/com/celebration/demo/model/entity/UserLocate.java
@@ -0,0 +1,14 @@
+package com.celebration.demo.model.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user_locate")
+public class UserLocate {
+}
